0c82a1a1e517f965e07ef4a2127f0ad606ab4ae4
SarthakBhawar <bhawarsarthak1812@gmail.com>
2024-11-16 22:47:45 +0530

    Create Assignment 6.cpp

---
+++ Assignment 6.cpp
@@ -0,0 +1,5 @@
+/*
+
+a c++ class name Tu that model numbers screen size data member to hold and price number function include overloaded insertion and execution operator If more than 4 digiks are entered for the model number than 12 or
+  negative or main function to enter is if screen size is omaller greater than 70 inches or if the price is over $5000 then throw that instantiates TV On integer write a objeck, allows users data & display the data
+members. If an exception caught replace all data members value with mero values 
